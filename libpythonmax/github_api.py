a7522ecb2a640127637dea2f938f49449fb3252d
maxProgrammer <maxwneto.poo@gmail.com>
2021-08-24 21:28:01 -0300

    instalado biblioteca request e realizado consumo da api github para conseguir link do avatar do usuário.

---
+++ libpythonmax/github_api.py
@@ -0,0 +1,15 @@
+import requests
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuário no Github
+    :param usuario: sr com o nome de um usuário no github
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    res = requests.get(url)
+    return res.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('maxProgrammer'))
+
