59f57c069e6f1c2a12bb256d954e6f1e4d178b72
maxProgrammer <maxwneto.poo@gmail.com>
2021-09-06 23:41:18 -0300

    utilizado pytest-mock para realizar troca de bibliotecas

--- libpythonmax/tests/test_spam/test_github_api.py
+++ libpythonmax/tests/test_spam/test_github_api.py
@@ -6,7 +6,7 @@ from libpythonmax import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/87916631?v=4'
     resp_mock.json.return_value = {
@@ -14,10 +14,9 @@ def avatar_url():
         'id': 87916631,
         'avatar_url': url
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonmax.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
